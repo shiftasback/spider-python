2b5daecfc3dc5241854ca3093fadc5dde1932d1d
shiftasback <z13479634217@gmail.com>
2023-04-17 21:53:35 +0800

    更新 xiaoshuo.py

--- xiaoshuo.py
+++ xiaoshuo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from lxml import etree
 import requests
 import re
@@ -23,31 +22,3 @@ for i in strtext:
     f.write(str(i)+"\n")
 f.close()
 
-
-=======
-from lxml import etree
-import requests
-import re
-from lxml import etree
-
-url = "https://www.bbiquge.net/book/59265/57967084.html"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47"}
-page_text = requests.get(url=url, headers=headers, timeout=2000)
-page_text.encoding = page_text.apparent_encoding
-tree = etree.HTML(page_text.text)
-titlename = tree.xpath('//div[@class="border"]/h1/text()')
-listtext = tree.xpath('//div[@id="content"]/text()')
-listtext = [x.strip() for x in listtext]
-strtext = listtext
-print(titlename)
-print(strtext)
-
-f = open("sywz.txt", "a",encoding='utf-8')
-f.write(str(titlename)+"\n")
-for i in strtext:
-    f.write(str(i)+"\n")
-f.close()
-
-
->>>>>>> 91d23e864e8adda21527e125e1518ce6a80df637
