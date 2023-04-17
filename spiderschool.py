60bca4d0e1bf82ef612442a1e7c207914f10499b
shiftasback <z13479634217@gmail.com>
2023-04-17 21:50:09 +0800

    update
    小说网站

--- spiderschool.py
+++ spiderschool.py
@@ -5,57 +5,39 @@ import re
 import os
 import csv
 from bs4 import BeautifulSoup
-url = 'https://www.shanghairanking.cn/rankings/bcur/2022'
-headers = {
+def requestUrltext():
+    url = 'https://www.shanghairanking.cn/rankings/bcur/2022'
+    headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47"}
-page_text = requests.get(url = url,headers = headers,timeout=1000)
-page_text.encoding = page_text.apparent_encoding
-html = page_text.text
-page = page_text.text
-# print(page)
-soup = BeautifulSoup(html,'lxml')
-
-tree = etree.HTML(page_text.text)
+    page_text = requests.get(url=url, headers=headers, timeout=1000)
+    page_text.encoding = page_text.apparent_encoding
+    html_text = page_text
+    tree = etree.HTML(html_text.text)
+    return tree
 
 
-paiming = tree.xpath('//td/div/text()')
-paiming = [x.strip() for x in paiming]
-paiming = paiming[::2]
-# paiming = '<div class="ranking top1" data-v-3fe7d390>(.*?)</div>'
-# paiminglist = re.findall(paiming,html)
-# <td data-v-3fe7d390><div class="ranking top2" data-v-3fe7d390>               2             </div></td>
+def list_get(tree):
+    paiming = tree.xpath('//td/div/text()')
+    paiming = [x.strip() for x in paiming]
+    paiming =paiming[::2]
+    xuexiaomc = tree.xpath('//div/a[@class="name-cn"]/text()')
+    shengshi = tree.xpath('//td[@data-v-3fe7d390=""]/text()')
+    shengshi1 = [x.strip() for x in shengshi]
+    diqu = shengshi1[::4]
+    leixing = shengshi1[1::4]
+    zongfen = shengshi1[2::4]
+    banxuechengc = shengshi1[3::4]
+    print(len(shengshi))
+    return paiming,xuexiaomc,diqu,leixing,zongfen,banxuechengc
 
 
-# paiming = tree.xpath('//div[@class="ranking"]/text()')
-# paiming = [x.strip() for x in paiming]
-# for i in range(1,4):
-#         paiming.insert(i-1,str(i))
+def csv_return(paiming,xuexiaomc, diqu, leixing,zongfen,banxuechengc):
+    Data = pd.DataFrame(
+            { '排名':paiming,'学校名称':xuexiaomc,'地区': diqu, '类型': leixing, '总分': zongfen, '办学层次': banxuechengc})
 
+    Data.to_csv('spiderdata.csv', index=None,encoding='gbk')
+    return ("xieru")
 
-
-
-print('排名',paiming)
-# print(paiming)
-xuexiaomc = tree.xpath('//a[@class="name-cn"]/text()')
-print((xuexiaomc))
-shengshi = tree.xpath('//td[@data-v-3fe7d390=""]/text()')
-
-shengshi1 = [x.strip() for x in shengshi]
-# print(len(shengshi1))
-# print(shengshi1)
-diqu = shengshi1[::4]
-leixing = shengshi1[1::4]
-zongfen = shengshi1[2::4]
-banxuechengc = shengshi1[3::4]
-# print(diqu)
-# print(leixing)
-# print(zongfen)
-print(banxuechengc)
-print(len(diqu))
-print(len(leixing))
-print(len(zongfen))
-print(len(banxuechengc))
-Data = pd.DataFrame(
-        { '排名':paiming,'学校名称':xuexiaomc,'地区': diqu, '类型': leixing, '总分': zongfen, '办学层次': banxuechengc})
-
-Data.to_csv('spiderdata.school.csv', index=None,encoding='gbk')
+a = requestUrltext()
+paiming,xuexiaomc,diqu,leixing,zongfen,banxuechengc = list_get(a)
+csv_return(paiming,xuexiaomc,diqu,leixing,zongfen,banxuechengc)
